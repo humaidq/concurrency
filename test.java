aebf1659a16a910eead1c8e1f630c68c92b45653
Euan Gordon <48190058+cheerypal@users.noreply.github.com>
2020-02-07 20:44:45 +0000

    Create test.java

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test{
+  private static final string MESSAGE_TO_YASEEN = "Don't worry bro we can delete all these files";
+  private static final string MESSAGE_CONT = "HOPE WEDNESDAY GOES WELL";
+  public test(){
+    super(this);
+  }
+  public static void main(String[] args){
+    System.out.println(MESSAGE_TO_YASEEN + " " + MESSAGE_CONT);
+  }
+
+}
